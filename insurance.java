8a9ee533f7e4075d81d50d8793d8662d84d40e4d
Alexstericris <42732998+Alexstericris@users.noreply.github.com>
2018-11-05 11:06:42 +0100

    Create insurance.java

---
+++ insurance.java
@@ -0,0 +1,24 @@
+package dudaaa;
+
+public class insurance {
+
+	private String policyName;
+	private String policyId;
+
+	public String getPolicyName() {
+		return policyName;
+	}
+
+	public void setPolicyName(String policyName) {
+		this.policyName = policyName;
+	}
+
+	public String getPolicyId() {
+		return policyId;
+	}
+
+	public void setPolicyId(String policyId) {
+		this.policyId = policyId;
+	}
+
+}
